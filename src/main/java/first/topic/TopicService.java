47032a723f07e6bedc94fb7c8c4ad3b483ccc085
hindia <mahmmudhindia@gmail.com>
2018-10-14 10:37:04 +0300

    get api

--- src/main/java/first/topic/TopicService.java
+++ src/main/java/first/topic/TopicService.java
@@ -12,5 +12,10 @@ public class TopicService {
 			new Topic("id2","spring2","description2"),
 			new Topic("id3","spring3","description3")				
 			);
+	
 	public List<Topic> getAllTopics(){ return topics;}
+	
+	public Topic getTopic(String id){
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();	//iterates in the list to find the id that matches with the argument id in an elegant waay
+	}
 }

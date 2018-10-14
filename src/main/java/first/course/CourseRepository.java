7b9cbb614812bcad27cf57172ce2edb791cafeb6
hindia <mahmmudhindia@gmail.com>
2018-10-14 18:17:06 +0300

    added maven plug-in, deployable as a jar file as of now

--- src/main/java/first/course/CourseRepository.java
+++ src/main/java/first/course/CourseRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
-import first.topic.Topic;
 
 public interface CourseRepository extends CrudRepository<Course, String>{
 	public List<Course> findByTopicId(String topicId);
